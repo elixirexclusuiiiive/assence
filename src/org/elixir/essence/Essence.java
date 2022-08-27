Kangbangrom/Elixir_packages_apps_Essence

Essence: Init Our Feature Extension
All Credit goes to AEX Team and Devs

Co-authored-by: maxwen <max.weninger@gmail.com>
Co-authored-by: ishubhamsingh <coolsks94@gmail.com>
Co-authored-by: mydongistiny <jaysonedson@gmail.com>
Co-authored-by: SagarMakhar <sagarmakhar@gmail.com>
Co-authored-by: mome13 <mehran13mome@gmail.com>

src/org/elixir/essence/Essence.java
@@ -1,0 +1,249 @@
+/*
+ * Copyright (C) 2014-2016 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.elixir.essence;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.viewpager.widget.ViewPager;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import github.com.st235.lib_expandablebottombar.ExpandableBottomBar;
+
+import org.elixir.essence.categories.Lockscreen;
+import org.elixir.essence.categories.StatusBar;
+import org.elixir.essence.categories.System;
+import org.elixir.essence.categories.Hardware;
+
+import org.elixir.essence.navigation.BubbleNavigationConstraintView;
+import org.elixir.essence.navigation.BubbleNavigationChangeListener;
+
+public class Essence extends SettingsPreferenceFragment implements   
+       Preference.OnPreferenceChangeListener {
+
+    private static final int MENU_HELP  = 0;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        Resources res = getResources();
+
+        View view = inflater.inflate(R.layout.layout_extensions, container, false);
+
+        BubbleNavigationConstraintView bubbleNavigationConstraintView =  (BubbleNavigationConstraintView) view.findViewById(R.id.floating_top_bar_navigation);
+        ViewPager viewPager = view.findViewById(R.id.viewpager);
+        PagerAdapter mPagerAdapter = new PagerAdapter(getFragmentManager());
+        viewPager.setAdapter(mPagerAdapter);
+
+        bubbleNavigationConstraintView.setNavigationChangeListener(new BubbleNavigationChangeListener() {
+            @Override
+            public void onNavigationChanged(View view, int position) {
+                if (view.getId() == R.id.status_bar_category) {
+                    viewPager.setCurrentItem(position, true);
+                } else if (view.getId() == R.id.lockscreen_category) {
+                    viewPager.setCurrentItem(position, true);
+                } else if (view.getId() == R.id.system_category) {
+                    viewPager.setCurrentItem(position, true);
+                } else if (view.getId() == R.id.hardware_category) {
+                    viewPager.setCurrentItem(position, true);
+                }
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                bubbleNavigationConstraintView.setCurrentActiveItem(i);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+            }
+        });
+
+        setHasOptionsMenu(true);
+
+        return view;
+    }
+
+    class PagerAdapter extends FragmentPagerAdapter {
+
+        String titles[] = getTitles();
+        private Fragment frags[] = new Fragment[titles.length];
+
+        PagerAdapter(FragmentManager fm) {
+            super(fm);
+            frags[0] = new StatusBar();
+            frags[1] = new Lockscreen();
+            frags[2] = new System();
+            frags[3] = new Hardware();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return frags[position];
+        }
+
+        @Override
+        public int getCount() {
+            return frags.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+    }
+
+    private String[] getTitles() {
+        String titleString[];
+        titleString = new String[]{
+            getString(R.string.status_bar_category),
+            getString(R.string.lockscreen_category),
+            getString(R.string.system_category),
+            getString(R.string.hardware_category)};
+
+        return titleString;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActivity().setTitle(R.string.extensions_title);
+        ContentResolver resolver = getActivity().getContentResolver();
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.CUSTOM_SETTINGS;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        return true;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.add(0, MENU_HELP, 0, R.string.extensions_dialog_title)
+                .setIcon(R.drawable.ic_extensions_info)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_HELP:
+                showDialogInner(MENU_HELP);
+                Toast.makeText(getActivity(),
+                (R.string.extensions_dialog_toast),
+                Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private void showDialogInner(int id) {
+        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
+        newFragment.setTargetFragment(this, 0);
+        newFragment.show(getFragmentManager(), "dialog " + id);
+    }
+
+    public static class MyAlertDialogFragment extends DialogFragment {
+
+        public static MyAlertDialogFragment newInstance(int id) {
+            MyAlertDialogFragment frag = new MyAlertDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt("id", id);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            int id = getArguments().getInt("id");
+            switch (id) {
+                case MENU_HELP:
+                    return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.extensions_dialog_title)
+                    .setMessage(R.string.extensions_dialog_message)
+                    .setCancelable(false)
+                    .setNegativeButton(R.string.dlg_ok,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    })
+                    .create();
+            }
+            throw new IllegalArgumentException("unknown id " + id);
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+
+        }
+    }
+}
+

elixirexclusuiiiive/assence

Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>
Co-Authored-By: Nico60 <ndhouailly@gmail.com>
Co-Authored-By: SKULSHADY <anushekprasal@gmail.com>
Co-Authored-By: Arghya Chanda <arghyac35@gmail.com>
Co-Authored-By: spezi77 <spezi7713@gmx.net>
Co-Authored-By: ShubhamB <shubhamprince111@gmail.com>
Co-Authored-By: SuperDroidBond <superdroidbond@yahoo.com>
Co-Authored-By: jhonboy121 <alfredmathew05@gmail.com>
Co-Authored-By: idoybh <idoybh2@gmail.com>
Co-Authored-By: Adithya <gh0strider.2k18.reborn@gmail.com>
Co-Authored-By: Rituj Beniwal <ritujbeniwal@gmail.com>
Co-Authored-By: Hernán Castañón Álvarez <herna@paranoidandroid.co>
Co-Authored-By: Akash Srivastava <akashniki@gmail.com>
Co-Authored-By: maxwen <max.weninger@gmail.com>
Co-Authored-By: ishubhamsingh <coolsks94@gmail.com>
Co-Authored-By: mydongistiny <jaysonedson@gmail.com>
Co-Authored-By: SagarMakhar <sagarmakhar@gmail.com>
Co-Authored-By: mome13 <mehran13mome@gmail.com>

src/org/elixir/essence/Essence.java
@@ -1,0 +1,259 @@
+/*
+ * Copyright (C) 2014-2016 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.elixir.essence;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.viewpager.widget.ViewPager;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import github.com.st235.lib_expandablebottombar.ExpandableBottomBar;
+
+import org.elixir.essence.categories.Lockscreen;
+import org.elixir.essence.categories.StatusBar;
+import org.elixir.essence.categories.Themes;
+import org.elixir.essence.categories.Qs;
+import org.elixir.essence.categories.System;
+import org.elixir.essence.categories.Hardware;
+
+import org.elixir.essence.navigation.BubbleNavigationConstraintView;
+import org.elixir.essence.navigation.BubbleNavigationChangeListener;
+
+public class Essence extends SettingsPreferenceFragment implements   
+       Preference.OnPreferenceChangeListener {
+
+    private static final int MENU_HELP  = 0;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        Resources res = getResources();
+
+        View view = inflater.inflate(R.layout.layout_extensions, container, false);
+
+        BubbleNavigationConstraintView bubbleNavigationConstraintView =  (BubbleNavigationConstraintView) view.findViewById(R.id.floating_top_bar_navigation);
+        ViewPager viewPager = view.findViewById(R.id.viewpager);
+        PagerAdapter mPagerAdapter = new PagerAdapter(getFragmentManager());
+        viewPager.setAdapter(mPagerAdapter);
+
+        bubbleNavigationConstraintView.setNavigationChangeListener(new BubbleNavigationChangeListener() {
+            @Override
+            public void onNavigationChanged(View view, int position) {
+                if (view.getId() == R.id.system_category) {
+                    viewPager.setCurrentItem(position, true);
+		} else if (view.getId() == R.id.themes_category) {
+                    viewPager.setCurrentItem(position, true);
+                } else if (view.getId() == R.id.status_bar_category) {
+                    viewPager.setCurrentItem(position, true);
+                } else if (view.getId() == R.id.qs_category) {
+                    viewPager.setCurrentItem(position, true);
+		} else if (view.getId() == R.id.lockscreen_category) {
+                    viewPager.setCurrentItem(position, true);
+                } else if (view.getId() == R.id.hardware_category) {
+                    viewPager.setCurrentItem(position, true);
+                }
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                bubbleNavigationConstraintView.setCurrentActiveItem(i);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+            }
+        });
+
+        setHasOptionsMenu(true);
+
+        return view;
+    }
+
+    class PagerAdapter extends FragmentPagerAdapter {
+
+        String titles[] = getTitles();
+        private Fragment frags[] = new Fragment[titles.length];
+
+        PagerAdapter(FragmentManager fm) {
+            super(fm);
+            frags[0] = new System();
+	    frags[1] = new Themes();
+            frags[2] = new StatusBar();
+	    frags[3] = new Qs();
+            frags[4] = new Lockscreen();
+            frags[5] = new Hardware();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return frags[position];
+        }
+
+        @Override
+        public int getCount() {
+            return frags.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+    }
+
+    private String[] getTitles() {
+        String titleString[];
+        titleString = new String[]{
+            getString(R.string.system_category),
+	    getString(R.string.themes_category),
+            getString(R.string.status_bar_category),
+            getString(R.string.qs_category),
+	    getString(R.string.lockscreen_category),
+            getString(R.string.hardware_category)};
+		
+        return titleString;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActivity().setTitle(R.string.extensions_title);
+        ContentResolver resolver = getActivity().getContentResolver();
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.CUSTOM_SETTINGS;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        return true;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.add(0, MENU_HELP, 0, R.string.extensions_dialog_title)
+                .setIcon(R.drawable.ic_extensions_info)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_HELP:
+                showDialogInner(MENU_HELP);
+                Toast.makeText(getActivity(),
+                (R.string.extensions_dialog_toast),
+                Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private void showDialogInner(int id) {
+        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
+        newFragment.setTargetFragment(this, 0);
+        newFragment.show(getFragmentManager(), "dialog " + id);
+    }
+
+    public static class MyAlertDialogFragment extends DialogFragment {
+
+        public static MyAlertDialogFragment newInstance(int id) {
+            MyAlertDialogFragment frag = new MyAlertDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt("id", id);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            int id = getArguments().getInt("id");
+            switch (id) {
+                case MENU_HELP:
+                    return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.extensions_dialog_title)
+                    .setMessage(R.string.extensions_dialog_message)
+                    .setCancelable(false)
+                    .setNegativeButton(R.string.dlg_ok,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    })
+                    .create();
+            }
+            throw new IllegalArgumentException("unknown id " + id);
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+
+        }
+    }
+}
+